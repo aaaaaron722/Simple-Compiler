3ee3ba5d552919b1418d39b182961b7b0e62d901
Aaron Lin <90635996+aaaaaron722@users.noreply.github.com>
2024-12-25 09:08:20 +0800

    Delete out.py

--- out.py
+++
@@ -1,7 +0,0 @@
-import sys
-print("Here is example 4: Input a number whether it greater than 5 or not?")
-num=int(input())
-if num<10 and num>=5:
-    print("Your input between 5 and 10")
-else:
-    print("Your input less than 5")
